7e7d15d86c656d894cfbcf90b98be16345af7db5
tracer-nju <tracer@nju.edu.cn>
2025-07-20 21:52:19 +0800

    Linux Naraka02 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:52:19 up 0 min,  1 user,  load average: 0.30, 0.07, 0.02

--- .shadow/sperf/tests.c
+++ .shadow/sperf/tests.c
@@ -3,27 +3,3 @@
 
 // TestKit lacks support of fork().
 // Write unit tests here.
-
-int parse_strace_line(char *line, char *syscall_name, double *time);
-
-UnitTest(test_parse_strace_line) {
-    char line[] = "openat(AT_FDCWD, \"/etc/ld.so.cache\", O_RDONLY|O_CLOEXEC) = 3 <0.000123>";
-    char syscall_name[64];
-    double time;
-    
-    int result = parse_strace_line(line, syscall_name, &time);
-    
-    tk_assert(result == 1, "Should successfully parse strace line");
-    tk_assert(strcmp(syscall_name, "openat") == 0, "Should extract correct syscall name");
-    tk_assert(time == 0.000123, "Should extract correct time");
-}
-
-UnitTest(test_parse_strace_line_no_time) {
-    char line[] = "write(1, \"hello\\n\", 6) = 6";
-    char syscall_name[64];
-    double time;
-    
-    int result = parse_strace_line(line, syscall_name, &time);
-    
-    tk_assert(result == 0, "Should fail to parse line without timing information");
-}
