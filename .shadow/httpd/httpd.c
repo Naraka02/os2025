df98c437b821c5e1bf105653db8a73fdb9f640b8
tracer-nju <tracer@nju.edu.cn>
2025-08-04 23:19:59 +0800

    Linux Naraka02 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:19:59 up  2:20,  1 user,  load average: 0.00, 0.05, 0.07

--- .shadow/httpd/httpd.c
+++ .shadow/httpd/httpd.c
@@ -176,7 +176,7 @@ void handle_request(int client_socket) {
 
     if (pid == 0) {
         // Child process: close the read end of the pipe
-        close(pipe_fd[0]);
+        //close(pipe_fd[0]);
 
         // Redirect stdout to the write end of the pipe
         dup2(pipe_fd[1], STDOUT_FILENO);
@@ -209,7 +209,7 @@ void handle_request(int client_socket) {
         waitpid(pid, &status, 0);
 
         if (WIFEXITED(status) != 0) {
-            status_code =WIFEXITED(status);
+            status_code = 501;
             goto send_error;
         }
 
