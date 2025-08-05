1ae3de24f8afd4b31ad99882591deb8c099f0eec
tracer-nju <tracer@nju.edu.cn>
2025-08-06 01:49:52 +0800

    Linux Naraka02 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:49:52 up  4:52,  1 user,  load average: 0.01, 0.05, 0.07

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -301,8 +301,7 @@ void extract_bmp(uint32_t cluster_num) {
             char sha1_str[41];
             calculate_sha1(file_data, bytes_read, sha1_str);
             
-            printf("%s  %s (offset=%u, %dx%d, size=%u)\n", 
-                   sha1_str, long_filename, bitmap_offset, width, abs(height), actual_file_size);
+            printf("%s  %s\n", sha1_str, long_filename);
             fflush(stdout);
         }
         
