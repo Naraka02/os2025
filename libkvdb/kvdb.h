8e0bf9d0b641bf22c3419f2afd2d5f9ae6dab890
Naraka_02 <naraka02@foxmail.com>
2025-08-06 23:06:35 +0800

    finish: libkvdb

--- libkvdb/kvdb.h
+++ libkvdb/kvdb.h
@@ -1,10 +1,29 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/file.h>
+#include <pthread.h>
 
-// Modify this struct.
+#define MAX_KEY_LEN 256
+#define MAX_VALUE_LEN 1024
+#define LOG_ENTRY_SIZE (MAX_KEY_LEN + MAX_VALUE_LEN + sizeof(int) * 2)
+
+typedef struct log_entry {
+    int key_len;
+    int value_len;
+    char key[MAX_KEY_LEN];
+    char value[MAX_VALUE_LEN];
+    int committed; // 0: not committed, 1: committed
+} log_entry_t;
+
 struct kvdb_t {
     const char *path;
     int fd;
+    pthread_mutex_t mutex;
 };
 
 int kvdb_open(struct kvdb_t *db, const char *path);
