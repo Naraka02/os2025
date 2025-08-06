fda95a6ed397aca681552055dc50209c0f552ec1
tracer-nju <tracer@nju.edu.cn>
2025-08-06 23:02:45 +0800

    Linux Naraka02 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:02:45 up  2:08,  1 user,  load average: 0.25, 0.20, 0.12

--- .shadow/libkvdb/tests/test_main.c
+++ .shadow/libkvdb/tests/test_main.c
@@ -14,7 +14,7 @@ SystemTest(test_kvdb_put_get, ((const char *[]){})) {
     tk_assert(kvdb_put(&db, "key", "value") == 0, "Must put key");
     char *buf[1024];
     tk_assert(kvdb_get(&db, "key", buf, 0) == 0, "Must get key");
-    tk_assert(strcmp(buf, "key") == 0, "key");
+    tk_assert(strcmp(buf, "key") == 0, "key: %s", buf);
     tk_assert(kvdb_close(&db) == 0, "Must close db");
 }
 
